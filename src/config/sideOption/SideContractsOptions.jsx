5354eeaa1e8f13bd8926f6a6d133a1ebe35477cc
giirrr <ikl1121@naver.com>
2024-09-27 00:00:08 +0900

    Style: 답변,질문 내용 많을 경우 짤리는 오류해결 포커싱 시 색상 제거

--- src/config/sideOption/SideContractsOptions.jsx
+++ src/config/sideOption/SideContractsOptions.jsx
@@ -4,16 +4,17 @@ import ChatbotHeader from "../../components/chatbot/ChatbotHeader";
 const SideContractsOptions = (props) => {
   const botAvatar = process.env.PUBLIC_URL + "/assets/avatar.png";
   const rightArrow = process.env.PUBLIC_URL + "/assets/right_arrow_18.png";
-  //  const
   const options = props.options || [{}];
 
   const optionsMarkup = options.map((option, index) => (
     <div
-      class="flex h-[34px]  w-[368px] cursor-pointer justify-between bg-Fill-Gray-Assistive p-2 text-xs text-Text-Normal hover:bg-Fill-Gray-Strong"
+      class="flex h-[34px] w-full cursor-pointer justify-between bg-Fill-Gray-Assistive p-2 text-xs text-Text-Normal hover:bg-Fill-Gray-Strong"
       key={index}
       onClick={() => props.actionProvider[option.handler]()}
     >
-      <div>{option.text}</div>
+      <div class="overflow-hidden text-ellipsis whitespace-nowrap ">
+        {option.text}
+      </div>
       <div class="flex h-[18px] w-[18px] items-center">
         <img src={rightArrow} />
       </div>
@@ -22,21 +23,17 @@ const SideContractsOptions = (props) => {
 
   return (
     <div class="mb-4 w-[200px]">
-      {/* <div class="mb-1 mt-4 h-full w-full pl-[22px]  text-xs font-normal leading-[18px] text-Text-Alternative">
-        선택하신 항목에 대해 상세하게 알려드릴께요!
-      </div> */}
-      {/* <div class="  bg-Primary-Dark"> */}
       <div class="flex">
         <img src={botAvatar} alt="Bot Avatar" className="chatbot-avatar" />
         <div>
           <ChatbotHeader />
-          <div class="ml-2 flex h-auto w-[400px] flex-col bg-white p-3">
-            <div class="mb-3 flex text-sm font-normal ">
-              질문을 선택해주세요
+          <div class="ml-2 flex h-auto w-[400px] flex-col rounded-[10px] bg-white p-3">
+            <div class="mb-3 flex text-sm font-normal">질문을 선택해주세요</div>
+            {/* 부모 div에 rounded-[8px]과 overflow-hidden 적용 */}
+            <div class="w-full overflow-hidden rounded-[9px]">
+              {optionsMarkup}
             </div>
-            {optionsMarkup}
           </div>
-          {/* </div> */}
         </div>
       </div>
     </div>
