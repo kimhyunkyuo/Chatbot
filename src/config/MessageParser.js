0db3f626c180b1f5a0ca7b593e0ccb8c5f15007f
giirrr <ikl1121@naver.com>
2024-09-27 00:05:49 +0900

    Fix: 사용자가 message parser 입력 시 커스텀메세지 중첩 오류 해결

--- src/config/MessageParser.js
+++ src/config/MessageParser.js
@@ -22,24 +22,24 @@ class MessageParser {
       (lowercase.includes("마케팅동의") && lowercase.includes("철회")) ||
       (lowercase.includes("마케팅 동의") && lowercase.includes("철회"))
     ) {
-      this.actionProvider.MarketingConsent();
+      this.actionProvider.MarketingConsentPar();
     } else if (lowercase.includes("중도인출") && lowercase.includes("신청")) {
-      this.actionProvider.IntermediateWithdrawal();
+      this.actionProvider.IntermediateWithdrawalPar();
     } else if (
       (lowercase.includes("만기보험금") && lowercase.includes("신청")) ||
       (lowercase.includes("만기 보험금") && lowercase.includes("신청"))
     ) {
-      this.actionProvider.MaturityInsuranceMoney();
+      this.actionProvider.MaturityInsuranceMoneyPar();
     } else if (lowercase.includes("지급금") && lowercase.includes("우편접수")) {
-      this.actionProvider.PaymentReceivedMail();
+      this.actionProvider.PaymentReceivedMailPar();
     } else if (lowercase.includes("해약") && lowercase.includes("신청")) {
-      this.actionProvider.Cancellation();
+      this.actionProvider.CancellationPar();
     } else if (
       lowercase.includes("보험료") &&
       lowercase.includes("미인출") &&
       lowercase.includes("청구")
     ) {
-      this.actionProvider.InsurancePremiumPayment();
+      this.actionProvider.InsurancePremiumPaymentPar();
     } else if (
       lowercase.includes("자동이체") &&
       lowercase.includes("변경") &&
@@ -57,28 +57,28 @@ class MessageParser {
       lowercase.includes("적립금") &&
       lowercase.includes("해약환급금")
     ) {
-      this.actionProvider.ReserveFund();
+      this.actionProvider.ReserveFundPar();
     } else if (lowercase.includes("해약환급금")) {
-      this.actionProvider.CancellationRefund();
+      this.actionProvider.CancellationRefundPar();
     } else if (lowercase.includes("보험증권") && lowercase.includes("신청")) {
-      this.actionProvider.SidePolicy();
+      this.actionProvider.SidePolicyPar();
     } else if (lowercase.includes("계약")) {
-      this.actionProvider.SideContractDetails();
+      this.actionProvider.SideContractDetailsPar();
     } else if (lowercase.includes("개인정보") && lowercase.includes("변경")) {
-      this.actionProvider.PersonalInformations();
+      this.actionProvider.PersonalInformationsPar();
     } else if (lowercase.includes("증명서") && lowercase.includes("발급")) {
-      this.actionProvider.Certificate();
+      this.actionProvider.CertificatePar();
     } else if (
       (lowercase.includes("보험금 청구") && lowercase.includes("신청서류")) ||
       (lowercase.includes("보험금청구") && lowercase.includes("신청서류"))
     ) {
-      this.actionProvider.InsuranceClaims();
+      this.actionProvider.InsuranceClaimsPar();
     } else if (
       lowercase.includes("청약철회") &&
       lowercase.includes("신청") &&
       lowercase.includes("절차")
     ) {
-      this.actionProvider.WithdrawalSubscription();
+      this.actionProvider.WithdrawalSubscriptionPar();
     } else if (
       lowercase.includes("보험가입") &&
       lowercase.includes("준비사항")
@@ -96,8 +96,6 @@ class MessageParser {
       this.actionProvider.InsurancePolicy1();
     } else if (lowercase.includes("보험가입") && lowercase.includes("보장")) {
       this.actionProvider.InsurancePolicyAfterPurchase1();
-    } else if (lowercase.includes("수익자")) {
-      this.actionProvider.SpecifyingBeneficiaries1();
     } else if (lowercase.includes("보험가입") && lowercase.includes("금액")) {
       this.actionProvider.AmountInsurance1();
     } else if (lowercase.includes("보험가입") && lowercase.includes("불가")) {
@@ -114,13 +112,15 @@ class MessageParser {
       lowercase.includes("변경") &&
       lowercase.includes("약정동의")
     ) {
-      this.actionProvider.SideInsuranceBeneficiary();
+      this.actionProvider.SideInsuranceBeneficiaryPar();
     } else if (
       lowercase.includes("수익자") &&
       lowercase.includes("법정상속인") &&
       lowercase.includes("상속순위")
     ) {
-      this.actionProvider.SideBeneficiary();
+      this.actionProvider.SideBeneficiaryPar();
+    } else if (lowercase.includes("수익자")) {
+      this.actionProvider.SpecifyingBeneficiaries1();
     } else {
       this.actionProvider.Error1();
     }
